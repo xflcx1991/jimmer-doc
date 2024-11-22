f4ff9ac7bd1077d07dc23b81bbdaa69321bf32c6
Chengxiang Liu <chengxiang@qcraft.ai>
2024-11-22 21:44:17 +0800

    🐞 fix: 修正部署问题
    1. 修正baseUrl
    2. 修改logo.png的链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,10 +9,10 @@ const config = {
   title: 'A revolutionary ORM framework for both java & kotlin',
   tagline: 'Not only ORM, but also a complete integrated solution',
   url: 'https://github.com',
-  baseUrl: '/jimmer-doc/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'https://raw.githubusercontent.com/babyfish-ct/jimmer-doc/main/logo.png',
+  favicon: '/img/logo.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -95,7 +95,7 @@ const config = {
       title: 'Jimmer documentation',
       logo: {
         alt: 'My Site Logo',
-        src: 'https://raw.githubusercontent.com/babyfish-ct/jimmer-doc/main/logo.png',
+        src: '/img/logo.png',
       },
       items: [
         {
